9aefa219707062e65e6228c39b4ae3ec94007dfb
adryansherman <adryansherman@gmail.com>
2019-07-29 18:58:11 +0700

    memulai tugas

---
+++ exercise-1.js
@@ -0,0 +1,55 @@
+
+
+A. Newton Second Law
+
+Algoritma
+1. Simpan "m" dengan nilai 600
+2. Simpan "a" dengan nilai 2
+3. Simpan  "F" tanpa ada nilai
+3. Hitung hasil kali variable 'm' dengan variable 'a'
+4. Simpan hasil Kali variable 'm' dengan variable 'a' di variable 'F'
+5. Tampilkan 'F'
+
+Pseudocode
+1. STORE 'm' with 600
+2. STORE 'a' with 2
+4. STORE 'F' without any value
+3. CALCULATE 'm' TIMES 'a'
+4. SET 'F' value as result from step 3
+5. DISPLAY 'F'
+
+B. Tahun kabisat
+
+Algoritma
+1. Simpan "tahun"  dengan nilai berapapun maksimal angka 4 digit
+2. Jika "tahun" habis dibagi 4 dan tidak habis dibagi 100, atau
+3. Jika "tahun" habis dibagi 4, habis dibagi 100 dan habis dibagi 400
+4 Tampilkan "tahun kabisat"
+5 Selain itu tampilkan "tahun normal"
+
+Pseudocodeq
+1. STORE "tahun" with any value with MAXIMUM 4 DIGITS
+2. IF "tahun" modulo 4 EQUALS TO 0 and "tahun" modulo 100 GREATER THAN 0, or
+3. IF "tahun" modulo 4, modulo by 100 and modulo by 400 EQUALS TO 0
+4. DISPLAY "tahun kabisat"
+
+C. Laundry D
+
+Algoritma
+1. Simpan "pakaian" dengan nilai angka 0
+2. Ketika "pakaian" nilainya sudah sama dengan 20 lanjut step 4
+3. Ulangi 
+
+Pseudocode
+1. STORE "pakaian" with 0
+2. WHILE 'Pakaian < 21
+3. ADD "pakaian" by 1
+4. DISPLAY "pakaian"
+
+D. PERIKSA KUKU
+
+Algoritma
+1. Simpan "kuku siswa" dengan nilai 0
+2. 
+
+
