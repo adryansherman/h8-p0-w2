f30e65d89fb77ae66a5bb0568c1c7a0130338138
adryansherman <adryansherman@gmail.com>
2019-08-06 21:39:18 +0700

    revisi exercise

--- exercise-9.js
+++ exercise-9.js
@@ -1,3 +1,4 @@
+//revisi hasil feedback;
 // tugas 1
 function shoutOut() {
     return 'Halo Function!';
@@ -8,27 +9,21 @@ console.log('====================')
 
 // tugas 2
 
-function calculateMultiply(){
-   return num1*num2;
+function calculateMultiply(num1, num2){
+   var hasilPerkalian = num1*num2;
+   return hasilPerkalian;
 }
-var num1 = 5;
-var num2 = 6;
 
-var hasilPerkalian = calculateMultiply(num1,num2);
-console.log(hasilPerkalian);
+console.log(calculateMultiply(5,6));
 
 console.log('====================')
 
 //tugas 3
 
-function processSentence(){
+function processSentence(name,age,address,hobby){
     return 'Nama saya '+ name + ', umur saya ' + age + ' tahun, alamat saya di ' + address + ' dan saya punya hobby yaitu ' + hobby + '!'
 }
 
-var name = "Agus";
-var age = 30;
-var address = "Jln. Malioboro, Yogjakarta";
-var hobby = "gaming";
 
-var fullSentence = processSentence(name,age,address,hobby);
-console.log(fullSentence);
+
+console.log(processSentence('Agus', 30, 'Jln. Malioboro, Yogjakarta', 'gaming'));
